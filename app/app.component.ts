8e18fe9441ba93bb66c85cc72e3119d85bcbf8fe
Chris Young <ctopheryoung@gmail.com>
2016-03-25 09:49:44 -0700

    implement meal display functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from 'angular2/core';
+import { MealListComponent } from './meal-list.component';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
+  directives: [MealListComponent],
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+  <div class="container">
+    <h1>Food Tracker</h1>
+    <meal-list
+      [mealList]="meals">
+    </meal-list>
+  </div>
   `
 })
-
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Bagel", "Plain bagel with cream cheese", 450, 0),
+      new Meal("Orange", "Small mandarin orange for a snack", 37, 1),
+      new Meal("Sandwich", "Turkey with cheese", 140, 2)
+    ]
+  }
 }
